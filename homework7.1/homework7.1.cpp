3d907005656a6c2ac6fbb6cf85665a5adc301964
Sergey <57405954+Sergey202222@users.noreply.github.com>
2025-03-25 23:33:58 +0300

    Add files via upload

--- homework7.1/homework7.1.cpp
+++ homework7.1/homework7.1.cpp
@@ -3,15 +3,6 @@
 
 int function(std::string str, int forbidden_length)
 {
-    try
-    {
-        if (str.length() == forbidden_length) throw str.length();
-    }
-    catch(size_t bad_length)
-    {
-        std::cerr << "Вы ввели слово запретной длины! До свидания";
-        
-    }
     return str.length();
 }
 
@@ -20,16 +11,23 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     std::string str{};
-    int forbidden_length{}, length{};
+    int forbidden_length{};
     std::cout << "Введите запретную длину: ";
     std::cin >> forbidden_length;
     while (true)
     {
         std::cout << "Введите слово: ";
         std::cin >> str;
-        length = function(str, forbidden_length);
-        if (length == forbidden_length) return EXIT_FAILURE;
-        std::cout << "Длина слова " << "\"" << str << "\" " << length << std::endl;
+        try
+        {
+            if (str.length() == forbidden_length) throw std::runtime_error("Вы ввели слово запретной длины! До свидания");
+        }
+        catch (std::exception& exp)
+        {
+            std::cerr << exp.what();
+            break;
+        }
+        std::cout << "Длина слова " << "\"" << str << "\" " << function(str, forbidden_length) << std::endl;
     }
     return EXIT_SUCCESS;
 }
